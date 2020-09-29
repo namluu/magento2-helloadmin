4e5c2551bfe02c1d591dd16056fdf409a0d1d378
Nam Luu <nam.luu@isobar.com>
2020-09-29 10:27:12 +0700

    admin - add grid ui component (not work)

---
+++ Controller/Adminhtml/Uindex/Index.php
@@ -0,0 +1,62 @@
+<?php
+namespace Namluu\Helloadmin\Controller\Adminhtml\Uindex;
+
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\App\ObjectManager;
+use Magento\Framework\App\Request\DataPersistorInterface;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends \Magento\Backend\App\Action implements HttpGetActionInterface
+{
+    /**
+     * Authorization level of a basic admin session
+     *
+     * @see _isAllowed()
+     */
+    const ADMIN_RESOURCE = 'Namluu_Helloadmin::index';
+
+    /**
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * @var DataPersistorInterface
+     */
+    private $dataPersistor;
+
+    /**
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     * @param DataPersistorInterface $dataPersistor
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory,
+        DataPersistorInterface $dataPersistor = null
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+        $this->dataPersistor = $dataPersistor ?: ObjectManager::getInstance()->get(DataPersistorInterface::class);
+    }
+
+    /**
+     * Index action
+     *
+     * @return \Magento\Backend\Model\View\Result\Page
+     */
+    public function execute()
+    {
+        /** @var \Magento\Backend\Model\View\Result\Page $resultPage */
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu('Namluu_Helloadmin::helloadmin_uindex');
+        $resultPage->addBreadcrumb(__('UIndex'), __('UIndex'));
+        $resultPage->addBreadcrumb(__('Manage Subscriptions'), __('Manage Subscriptions'));
+        $resultPage->getConfig()->getTitle()->prepend(__('Subscriptions'));
+
+        $this->dataPersistor->clear('helloadmin_uindex');
+
+        return $resultPage;
+    }
+}
---
+++ Ui/Component/DataProvider.php
@@ -0,0 +1,40 @@
+<?php
+namespace Namluu\Helloadmin\Ui\Component;
+
+use Magento\Framework\Api\FilterBuilder;
+use Magento\Framework\Api\Search\SearchCriteriaBuilder;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\View\Element\UiComponent\DataProvider\Reporting;
+
+class DataProvider extends \Magento\Framework\View\Element\UiComponent\DataProvider\DataProvider
+{
+    /**
+     * @param string $name
+     * @param string $primaryFieldName
+     * @param string $requestFieldName
+     * @param Reporting $reporting
+     * @param SearchCriteriaBuilder $searchCriteriaBuilder
+     * @param RequestInterface $request
+     * @param FilterBuilder $filterBuilder
+     * @SuppressWarnings(PHPMD.ExcessiveParameterList)
+     */
+    public function __construct(
+        $name,
+        $primaryFieldName,
+        $requestFieldName,
+        Reporting $reporting,
+        SearchCriteriaBuilder $searchCriteriaBuilder,
+        RequestInterface $request,
+        FilterBuilder $filterBuilder
+    ) {
+        parent::__construct(
+            $name,
+            $primaryFieldName,
+            $requestFieldName,
+            $reporting,
+            $searchCriteriaBuilder,
+            $request,
+            $filterBuilder
+        );
+    }
+}
